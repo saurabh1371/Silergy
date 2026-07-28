5c55937a62c802c7fe2f073b58fd958d4a935dad
Saurabh <er.saurabhrajput@gmail.com>
2026-07-28 14:18:27 +0530

    restructuring

--- src/SY7T108/projects/demo_dlms/ARM_keil/Objects/align_hex.bat
+++
@@ -1,6 +0,0 @@
-CD .\\Objects
-srec_cat.exe -Disable_Sequence_Warnings SY7T108_1p2w_shunt_ct_dlms_1.hex -Intel -fill 0xff 0x0000 0x200000 -o SY7T108_1p2w_shunt_ct_dlms_1_temp.hex -Intel --address_length=4 --line_length=44 -output_block_size=16
-srec_cat.exe -Disable_Sequence_Warnings SY7T108_1p2w_shunt_ct_dlms_1_temp.hex -Intel -unfill 0xFF 16 -o SY7T108_1p2w_shunt_ct_dlms_1_aligned.hex -Intel --address_length=4 --line_length=44 -output_block_size=16
-del SY7T108_1p2w_shunt_ct_dlms_1_temp.hex
-
-
--- src/_readme.txt
+++
@@ -1,9 +0,0 @@
-This is the source folder for a Silergy Meter Software Development Kit
-\cm0 has shared code for a Cortex M0.
-\drivers has the source code for some shared drivers.
-\library has some generic library code useful for the demo code.
-\sy*_<type><number> is a chip's private drivers and code.
-
-clean.bat click on it to delete files produced by many editors and compilers.
-
-$Id: _readme.txt 21883 2019-09-13 23:09:51Z rvandewa $
